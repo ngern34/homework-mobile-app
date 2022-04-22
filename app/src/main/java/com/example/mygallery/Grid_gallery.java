46ffca0f85c84433a49c16941b5598ecf0bc2210
phimonSTPP <monstpp99@gmail.com>
2022-04-22 11:44:19 +0700

    mon

---
+++ app/src/main/java/com/example/mygallery/Grid_gallery.java
@@ -0,0 +1,15 @@
+package com.example.mygallery;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Grid_gallery extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_grid);
+    }
+}
